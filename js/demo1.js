b67a3e7e04eba53d579a014b40d7d029922cf574
Noritaka Horio <holy.shared.design@gmail.com>
2010-10-27 23:06:20 +0900

    oops demo icon...
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- js/demo1.js
+++ js/demo1.js
@@ -11,7 +11,7 @@ window.addEvent('domready', function(){
 
 	var marker = map.factory('marker', {
 		point: point,
-		label: 'H',
+//		label: 'H',
 		icon: 'http://holyshared.github.com/StaticMaps/images/img_marker1.png'
 	});
 	map.addMarker(marker);
