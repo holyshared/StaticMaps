6e1bb71b9e951061aa8c220bf03cc5a492a1afed
Noritaka Horio <holy.shared.design@gmail.com>
2010-10-30 12:41:24 +0900

    The test file is added.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

---
+++ Tests/js/options.js
@@ -0,0 +1,29 @@
+(function($){
+
+	window.addEvent('domready', function(){
+
+		var options = {
+			map: {
+				size: { width: 600, height: 300 },
+				format: 'png32',
+				maptype: 'roadmap',
+				mobile: false,
+				language: 'en'
+			},
+			position: {
+				center:  '原美術館',
+				zoom: 15 
+			},
+			markers: [
+				{
+					point: '原美術館',
+					icon: 'http://holyshared.github.com/StaticMaps/images/img_marker1.png'
+				}
+			]
+		};
+
+		var map = new StaticMaps(options);
+		map.renderTo($('map'));
+	});
+
+}(document.id));
